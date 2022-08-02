317fbae2a97598a206476afa115a4b6263d102df
wuenzhe <2287710044@qq.com>
2022-08-02 16:17:42 +0800

    调整变量名称、进行单位换算

--- code.R
+++ code.R
@@ -3,6 +3,8 @@
 # 3. 缓释肥样本 + 氮肥深施度为0cm的样本（52）
 # 4. 合并变量及样本（92）
 
+rm(list = ls())
+
 # 设置工作路径
 getwd()
 setwd("D:/个人文件/Low Carbon/low carbon")
@@ -17,54 +19,64 @@ df1 <- read.xlsx("raw data.xlsx", sheetName = "depth", header = T)
 df2 <- read.xlsx("raw data.xlsx", sheetName = "slow_1", header = T)
 df3 <- read.xlsx("raw data.xlsx", sheetName = "slow_2", header = T)
 df4 <- read.xlsx("raw data.xlsx", sheetName = "merge", header = T)
+
+# 变量重命名
+names(df1)
+names(df1)[c(1: 10)] <- c("id", "CH4", "N2O", "CO2", "depth", "ratio", "densi", "irrig", "temper", "organ")
+names(df2)
+names(df2)[c(1: 10)] <- c("id", "CH4", "N2O", "CO2", "SRNF", "N", "densi", "irrig", "temper", "organ")
+names(df3)
+names(df3)[c(1: 10)] <- c("id", "CH4", "N2O", "CO2", "SRNF", "N", "densi", "irrig", "temper", "organ")
+names(df4)
+names(df4)[c(1: 12)] <- c("id", "CH4", "N2O", "CO2", "depth", "ratio", "N", "SRNF", "densi", "irrig", "temper", "organ")
+
+# 单位换算
+df2$N <- df2$N / 2
+df2$SRNF <- df2$SRNF / 2
+df3$N <- df3$N / 2
+df3$SRNF <- df3$SRNF / 2
+df4$N <- df4$N / 2
+df4$SRNF <- df4$SRNF / 2
+
+# 描述性统计
 summary(df1)
 summary(df2)
 summary(df3)
 summary(df4)
 
-# 变量重命名
-names(df1)
-names(df1)[c(1: 10)] <- c("id", "CH4", "N2O", "CO2", "depth", "ratio", "density", "irrigation", "temperature", "organics")
-names(df2)
-names(df2)[c(1: 10)] <- c("id", "CH4", "N2O", "CO2", "slow", "N", "density", "irrigation", "temperature", "organics")
-names(df3)
-names(df3)[c(1: 10)] <- c("id", "CH4", "N2O", "CO2", "slow", "N", "density", "irrigation", "temperature", "organics")
-names(df4)
-names(df4)[c(1: 12)] <- c("id", "CH4", "N2O", "CO2", "depth", "ratio", "N", "slow", "density", "irrigation", "temperature", "organics")
-
 # 回归分析
 ## depth
-depth_CH4 <- lm(CH4 ~ depth + ratio + density + irrigation + temperature + organics, data = df1)
+depth_CH4 <- lm(CH4 ~ depth + ratio + densi + irrig + temper + organ, data = df1)
 summary(depth_CH4)
-depth_N2O <- lm(N2O ~ depth + ratio + density + irrigation + temperature + organics, data = df1)
+depth_N2O <- lm(N2O ~ depth + ratio + densi + irrig + temper + organ, data = df1)
 summary(depth_N2O)
-depth_CO2 <- lm(CO2 ~ depth + ratio + density + irrigation + temperature + organics, data = df1)
+depth_CO2 <- lm(CO2 ~ depth + ratio + densi + irrig + temper + organ, data = df1)
 summary(depth_CO2)
-## slow_1
-slow_1_CH4 <- lm(CH4 ~ slow + N + density + irrigation + temperature + organics, data = df2)
-summary(slow_1_CH4)
-slow_1_N2O <- lm(N2O ~ slow + N + density + irrigation + temperature + organics, data = df2)
-summary(slow_1_N2O)
-slow_1_CO2 <- lm(CO2 ~ slow + N + density + irrigation + temperature + organics, data = df2)
-summary(slow_1_CO2)
-## slow_2
-slow_2_CH4 <- lm(CH4 ~ slow + N + density + irrigation + temperature + organics, data = df3)
-summary(slow_2_CH4)
-slow_2_N2O <- lm(N2O ~ slow + N + density + irrigation + temperature + organics, data = df3)
-summary(slow_2_N2O)
-slow_2_CO2 <- lm(CO2 ~ slow + N + density + irrigation + temperature + organics, data = df3)
-summary(slow_2_CO2)
+## SRNF_1
+SRNF_1_CH4 <- lm(CH4 ~ SRNF + N + densi + irrig + temper + organ, data = df2)
+summary(SRNF_1_CH4)
+SRNF_1_N2O <- lm(N2O ~ SRNF + N + densi + irrig + temper + organ, data = df2)
+summary(SRNF_1_N2O)
+SRNF_1_CO2 <- lm(CO2 ~ SRNF + N + densi + irrig + temper + organ, data = df2)
+summary(SRNF_1_CO2)
+## SRNF_2
+SRNF_2_CH4 <- lm(CH4 ~ SRNF + N + densi + irrig + temper + organ, data = df3)
+summary(SRNF_2_CH4)
+SRNF_2_N2O <- lm(N2O ~ SRNF + N + densi + irrig + temper + organ, data = df3)
+summary(SRNF_2_N2O)
+SRNF_2_CO2 <- lm(CO2 ~ SRNF + N + densi + irrig + temper + organ, data = df3)
+summary(SRNF_2_CO2)
 ## merge
-merge_CH4 <- lm(CH4 ~ depth + ratio + N + slow + density + irrigation + temperature + organics, data = df4)
+merge_CH4 <- lm(CH4 ~ depth + ratio + N + SRNF + densi + irrig + temper + organ, data = df4)
 summary(merge_CH4)
-merge_N2O <- lm(N2O ~ depth + ratio + N + slow + density + irrigation + temperature + organics, data = df4)
+merge_N2O <- lm(N2O ~ depth + ratio + N + SRNF + densi + irrig + temper + organ, data = df4)
 summary(merge_N2O)
-merge_CO2 <- lm(CO2 ~ depth + ratio + N + slow + density + irrigation + temperature + organics, data = df4)
+merge_CO2 <- lm(CO2 ~ depth + ratio + N + SRNF + densi + irrig + temper + organ, data = df4)
 summary(merge_CO2)
 
 # 输出结果
 stargazer(depth_CH4, depth_N2O, depth_CO2, title = "results", align = F, type = "text", no.space = TRUE, out = "depth.html")
-stargazer(slow_1_CH4, slow_1_N2O, slow_1_CO2, title = "results", align = F, type = "text", no.space = TRUE, out = "slow_1.html")
-stargazer(slow_2_CH4, slow_2_N2O, slow_2_CO2, title = "results", align = F, type = "text", no.space = TRUE, out = "slow_2.html")
+stargazer(SRNF_1_CH4, SRNF_1_N2O, SRNF_1_CO2, title = "results", align = F, type = "text", no.space = TRUE, out = "SRNF_1.html")
+stargazer(SRNF_2_CH4, SRNF_2_N2O, SRNF_2_CO2, title = "results", align = F, type = "text", no.space = TRUE, out = "SRNF_2.html")
 stargazer(merge_CH4, merge_N2O, merge_CO2, title = "results", align = F, type = "text", no.space = TRUE, out = "merge.html")
 
